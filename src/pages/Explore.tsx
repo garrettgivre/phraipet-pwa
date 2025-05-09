f37fbbb9ace7becc801524ce883982d73181237c
Garrett Brown <garrettgivre@gmail.com>
2025-05-09 00:20:59 -0500

    feat: clickable markers on infinite scroll map

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,2 +1,17 @@
-import FixedMapLayer from "../components/FixedMapLayer";
-export default function Explore() { return <FixedMapLayer />; }
+import InfiniteMap from "../components/InfiniteMap";
+import type { MapLocation } from "../components/InfiniteMap";
+
+const locations: MapLocation[] = [
+  {
+    id: "first-spot",
+    x: 600,
+    y: 300,
+    icon: "/icons/pink-dot.png",
+    onClick: () => alert("You clicked the first spot!")
+  },
+  // add more...
+];
+
+export default function Explore() {
+  return <InfiniteMap locations={locations} />;
+}
