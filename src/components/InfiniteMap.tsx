3b4392b741433db3fbca1ad582cd8fd1ae16494c
Garrett Brown <garrettgivre@gmail.com>
2025-05-09 00:41:48 -0500

    revert: restore simple infinite map before markers

--- src/components/InfiniteMap.tsx
+++ src/components/InfiniteMap.tsx
@@ -1,31 +1,17 @@
 import { useEffect, useRef } from "react";
 import "./InfiniteMap.css";
 
-/** 
- * Type-only export of a map marker spec 
- */
-export type MapLocation = {
-  id: string;
-  x: number;         // pixel offset from left of one map image
-  y: number;         // pixel offset from top of one map image
-  icon: string;      // e.g. "/icons/pink-dot.png"
-  onClick?: () => void;
-};
+const IMG_SRC = "/maps/world.png";
 
-interface InfiniteMapProps {
-  locations?: MapLocation[];
-}
-
-export default function InfiniteMap({ locations = [] }: InfiniteMapProps) {
+export default function InfiniteMap() {
   const scrollerRef = useRef<HTMLDivElement>(null);
   const imgWidthRef = useRef(0);
-  const IMG_SRC = "/maps/world.png";
 
   useEffect(() => {
     const scroller = scrollerRef.current;
     if (!scroller) return;
 
-    // Measure image width & jump to middle copy
+    // Measure the map width and jump to the middle copy
     const img = new Image();
     img.src = IMG_SRC;
     img.onload = () => {
@@ -33,11 +19,11 @@ export default function InfiniteMap({ locations = [] }: InfiniteMapProps) {
       scroller.scrollLeft = img.width;
     };
 
-    // Wrap-around logic
+    // Infinite wrap-around
     const onScroll = () => {
       const W = imgWidthRef.current;
-      if (scroller.scrollLeft <= 0) scroller.scrollLeft = W;
-      else if (scroller.scrollLeft >= W * 2) scroller.scrollLeft = W;
+      if (scroller.scrollLeft <= 0)      scroller.scrollLeft = W;
+      else if (scroller.scrollLeft >= W*2) scroller.scrollLeft = W;
     };
 
     scroller.addEventListener("scroll", onScroll);
@@ -46,30 +32,9 @@ export default function InfiniteMap({ locations = [] }: InfiniteMapProps) {
 
   return (
     <div className="mapScroller" ref={scrollerRef}>
-      {[0, 1, 2].map((tileIdx) => (
-        <div key={tileIdx} className="mapFrame">
-          <img
-            src={IMG_SRC}
-            alt="world map"
-            draggable={false}
-            className="mapImage"
-          />
-          {locations.map((loc) => (
-            <img
-              key={`${loc.id}-${tileIdx}`}
-              src={loc.icon}
-              draggable={false}
-              className="mapMarker"
-              style={{
-                left: loc.x + tileIdx * imgWidthRef.current,
-                top: loc.y,
-              }}
-              onClick={loc.onClick}
-              alt=""
-            />
-          ))}
-        </div>
-      ))}
+      <img src={IMG_SRC} alt="world map" draggable={false} />
+      <img src={IMG_SRC} alt="" aria-hidden="true" draggable={false} />
+      <img src={IMG_SRC} alt="" aria-hidden="true" draggable={false} />
     </div>
   );
 }
