84bb022af044d684cb855dbfb5dded64cf6f1abf
Garrett Brown <garrettgivre@gmail.com>
2025-05-09 20:37:50 -0500

    Map Stuff

--- src/components/FixedMapLayer.tsx
+++ src/components/FixedMapLayer.tsx
@@ -1,6 +1,11 @@
 import InfiniteMap from "./InfiniteMap";
+import type { Hotspot } from "./InfiniteMap";
 import "./FixedMapLayer.css";
 
-export default function FixedMapLayer() {
-  return <InfiniteMap />;
+interface FixedMapLayerProps {
+  hotspots?: Hotspot[];
+}
+
+export default function FixedMapLayer({ hotspots = [] }: FixedMapLayerProps) {
+  return <InfiniteMap hotspots={hotspots} />;
 }
