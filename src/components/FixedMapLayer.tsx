268dbdf57897b3c87bdb25c683f6c57078913385
Garrett Brown <garrettgivre@gmail.com>
2025-05-09 20:27:09 -0500

    Map Size

--- src/components/FixedMapLayer.tsx
+++ src/components/FixedMapLayer.tsx
@@ -2,9 +2,5 @@ import InfiniteMap from "./InfiniteMap";
 import "./FixedMapLayer.css";
 
 export default function FixedMapLayer() {
-  return (
-    <div className="fixedMapLayer">
-      <InfiniteMap />
-    </div>
-  );
+  return <InfiniteMap />;
 }
