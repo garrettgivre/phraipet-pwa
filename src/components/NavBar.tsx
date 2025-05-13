ebe307a78f4ee8048f38ac5b5fdb905a65f2b185
Garrett Brown <garrettgivre@gmail.com>
2025-05-13 01:00:35 -0500

    Nav Bubbles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,23 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./NavBar.css";
 
-const NavBar = () => (
-  <>
-    <hr className="divider" />
-    <nav className="nav">
-      <NavLink to="/" end>Pet</NavLink>
-      <NavLink to="/explore">Explore</NavLink>
-      <NavLink to="/play">Play</NavLink>
-    </nav>
-  </>
-);
+export default function NavBar() {
+  const navigate = useNavigate();
 
-export default NavBar;
+  return (
+    <nav className="nav-bar">
+      <div className="nav-item" onClick={() => navigate("/")}>
+        <img src="/assets/icons/bubble-b.png" alt="Pet" className="bubble-icon" />
+        <span>Pet</span>
+      </div>
+      <div className="nav-item" onClick={() => navigate("/explore")}>
+        <img src="/assets/icons/bubble-y.png" alt="Explore" className="bubble-icon" />
+        <span>Explore</span>
+      </div>
+      <div className="nav-item" onClick={() => navigate("/play")}>
+        <img src="/assets/icons/bubble-p.png" alt="Play" className="bubble-icon" />
+        <span>Play</span>
+      </div>
+    </nav>
+  );
+}
